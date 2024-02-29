c6352f28f113821c0cc3f585e3d537b40b887419
Igor Aleshichev <103876050+Aleshichev@users.noreply.github.com>
2024-02-29 10:26:06 +0000

    added Deckster's graph

--- 5_dict.py
+++ 5_dict.py
@@ -1,32 +1,90 @@
-# breadth first search  Поиск в ширину
-from collections import deque
+# # breadth first search  Поиск в ширину
 
+
+# #------------ graph --------------------
+# from collections import deque
+
+# graph = {}
+# graph["you"] = [ "alice", "bob", "claire"]
+# graph["bob"] = ["anuj", "peggy"]
+# graph["alice"] = ["peggy"]
+# graph["claire"] = ["thom", "jonny"]
+# graph["anuj"] = []
+# graph["peggy"] = []
+# graph["thom"] = []
+# graph["jonny"] = []
+
+# def person_is_seller(name):
+#     return name[-1] == 'm'
+
+# def search(name):
+#     search_queue = deque()
+#     search_queue += graph[name]
+#     searched = []
+#     while search_queue:
+#         person = search_queue.popleft()
+#         if not person in searched:
+#             if person_is_seller(person):
+#                 print(person +" is a mango seller!")
+#                 return True
+#             else:
+#                 search_queue += graph[person]
+#                 searched.append(person)
+#     return False
+
+# print(search("you"))
+
+
+#---------Deckster's graph ----------------------
 graph = {}
-graph["you"] = [ "alice", " bob", "claire"]
-graph["bob"] = ["anuj", "peggy"]
-graph["alice"] = ["peggy"]
-graph["claire"] = ["thom", "jonny"]
-graph["anuj"] = []
-graph["peggy"] = []
-graph["thom"] = []
-graph["jonny"] = []
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+graph["a"] = {}
+graph["a"]["fin"] = 1
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+graph["fin"] = {}
 
-def person_is_seller(name):
-    return name[-1] == 'm'
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
 
-def search(name):
-    search_queue = deque()
-    search_queue += graph[name]
-    searched = []
-    while search_queue:
-        person = search_queue.popleft()
-    if not person in searched:
-        if person_is_seller(person):
-            print (person +" is mango seller!")
-            return True
-        else:
-            search_queue += graph[person]
-            searched.append(person)
-    return False
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
 
-print(search("you"))
+# print(graph)
+# print(costs)
+# print(parents)
+
+processed = []
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys() :
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+
+
+print(processed)
